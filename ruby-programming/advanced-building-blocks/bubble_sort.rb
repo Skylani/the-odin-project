d31770ba1922eff99852713c58b78fc2f1e7cf58
Lani Huang <laniywh@gmail.com>
2016-04-20 10:09:11 -0700

    combine bubble sort methods

--- ruby-programming/advanced-building-blocks/bubble_sort.rb
+++ ruby-programming/advanced-building-blocks/bubble_sort.rb
@@ -23,5 +23,33 @@ def bubble_sort array
   array
 end
 
+# bubble sort that accept a block
+def bubble_sort_by array
+
+  sorted = false
+
+  loop do
+    if !sorted
+      # assume sorted for current loop
+      sorted = true
+      (0..array.length-2).each do |i|
+
+        if yield(array[i], array[i+1]) > 0
+          temp = array[i]
+          array[i] = array[i+1]
+          array[i+1] = temp
+          sorted = false # mark current loop !sorted
+        end
+      end
+    else
+      break
+
+    end
+  end
+  array
+end
+
 print bubble_sort([4,3,78,2,0,2])
+puts
+print bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length }
 puts
