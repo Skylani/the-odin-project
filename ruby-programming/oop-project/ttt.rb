9b9630e78895ed7e99e3ca4f6e642817c6f8512f
Lani Huang <laniywh@gmail.com>
2016-04-30 12:01:33 -0700

    full

--- ruby-programming/oop-project/ttt.rb
+++ ruby-programming/oop-project/ttt.rb
@@ -9,19 +9,61 @@ class Ttt
 
   def drawGrid
     puts
-    for i in 1..9
-      if i % 3 == 0
-        puts " #{i} "
-        puts "---+---+---" if i != 9
-      else
-        print " #{i} |"
+
+    # print grid
+    if !@p1.empty?
+      for i in 1..9
+        # determine move of current space
+        if @p1.include?(i)
+          move = 'x'
+        elsif @p2.include?(i)
+          move = 'o'
+        else
+          move = ' '
+        end
+
+        if i % 3 != 0
+          print " #{move} |"
+        else
+          puts " #{move} "
+          puts "---+---+---" if i != 9
+        end
+      end
+
+    # print initial grid w/ coordinate
+    else
+      for i in 1..9
+        if i % 3 != 0
+          print " #{i} |"
+        else
+          puts " #{i} "
+          puts "---+---+---" if i != 9
+        end
       end
     end
     puts
   end
 
-  def win?
+  def win?(player)
+    win_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                [1, 5, 9], [3, 5, 7]]
 
+    win_list.each do |set|
+      win = true
+      set.each do |x|
+        if !player.include?(x)
+          win = false
+          break
+        end
+      end
+      return true if win
+    end
+    false
+  end
+
+  def no_more_moves?
+    @p1.size + @p2.size == 9
   end
 
 end
@@ -29,30 +71,48 @@ end
 
 loop do
   # new game
+  puts 'New game!'
   game = Ttt.new
-  win = false
   current_player = 1
+  input = 0
 
-  while !win
-    # wait for player input
-    print "Player #{current_player}: "
+  loop do
 
     # get player input
-    input = gets.chomp
-    input = input.to_i
+    loop do
+      print "Player #{current_player}: "
+
+      input = gets.chomp
+      input = input.to_i
+
+      if game.p1.include?(input) || game.p2.include?(input) || input < 1 || input > 9
+        puts 'Invalid input! Try again.'
+      else
+        break
+      end
+    end
+
+    # save input
     current_player == 1 ? game.p1 << input : game.p2 << input
 
     # update board
     game.drawGrid
 
     # win?
-    if game.win?
-      win = true
-      puts "Player #{current_player} win!"
+    if game.win?(current_player == 1 ? game.p1 : game.p2)
+      puts "Result: Player #{current_player} win!"
+      break
+    # draw?
+    elsif game.no_more_moves?
+      puts 'Result: draw'
       break
     else
       current_player == 1 ? current_player = 2 : current_player = 1
     end
 
   end
+
+  puts 'Play again? [y/n]'
+  input = gets.chomp
+  break if input == 'n'
 end
