06bb1c5175c06b3bf5af87c402e28e7aff1f01aa
Lani Huang <laniywh@gmail.com>
2016-05-01 10:07:20 -0700

    object oriented

--- ruby-programming/oop-project/ttt.rb
+++ ruby-programming/oop-project/ttt.rb
@@ -1,47 +1,47 @@
-class Ttt
-  attr_accessor :p1, :p2
+class Grid
 
   def initialize
-    @p1 = []
-    @p2 = []
-    drawGrid
-  end
-
-  def drawGrid
     puts
-
-    # print grid
-    if !@p1.empty?
-      for i in 1..9
-        # determine move of current space
-        if @p1.include?(i)
-          move = 'x'
-        elsif @p2.include?(i)
-          move = 'o'
-        else
-          move = ' '
-        end
-
-        if i % 3 != 0
-          print " #{move} |"
-        else
-          puts " #{move} "
-          puts "---+---+---" if i != 9
-        end
-      end
-
-    # print initial grid w/ coordinate
-    else
-      for i in 1..9
-        if i % 3 != 0
-          print " #{i} |"
-        else
-          puts " #{i} "
-          puts "---+---+---" if i != 9
-        end
+    for i in 1..9
+      if i % 3 != 0
+        print " #{i} |"
+      else
+        puts " #{i} "
+        puts "---+---+---" if i != 9
       end
     end
     puts
+  end
+
+  def draw(p1, p2)
+    puts
+    for i in 1..9
+      # determine move of current space
+      if p1.include?(i)
+        move = 'x'
+      elsif p2.include?(i)
+        move = 'o'
+      else
+        move = ' '
+      end
+
+      # print cell
+      if i % 3 != 0
+        print " #{move} |"
+      else
+        puts " #{move} "
+        puts "---+---+---" if i != 9
+      end
+    end
+    puts
+  end
+
+end
+
+class Ttt
+  def initialize
+    @p1 = []
+    @p2 = []
   end
 
   def win?(player)
@@ -66,51 +66,55 @@ class Ttt
     @p1.size + @p2.size == 9
   end
 
-end
+  def play
+    @grid = Grid.new
+    current_player = 1
+    input = 0
 
+    loop do
+      # get player input
+      loop do
+        print "Player #{current_player}: "
+
+        input = gets.chomp
+        input = input.to_i
+
+        if @p1.include?(input) || @p2.include?(input) || input < 1 || input > 9
+          puts 'Invalid input! Try again.'
+        else
+          break
+        end
+      end
+
+      # save input
+      current_player == 1 ? @p1 << input : @p2 << input
+
+      # update board
+      @grid.draw(@p1, @p2)
+
+      # win?
+      if win?(current_player == 1 ? @p1 : @p2)
+        puts "Result: Player #{current_player} win!"
+        break
+      # draw?
+      elsif no_more_moves?
+        puts 'Result: draw'
+        break
+      else
+        current_player == 1 ? current_player = 2 : current_player = 1
+      end
+
+    end
+
+  end
+
+end
 
 loop do
   # new game
   puts 'New game!'
   game = Ttt.new
-  current_player = 1
-  input = 0
-
-  loop do
-
-    # get player input
-    loop do
-      print "Player #{current_player}: "
-
-      input = gets.chomp
-      input = input.to_i
-
-      if game.p1.include?(input) || game.p2.include?(input) || input < 1 || input > 9
-        puts 'Invalid input! Try again.'
-      else
-        break
-      end
-    end
-
-    # save input
-    current_player == 1 ? game.p1 << input : game.p2 << input
-
-    # update board
-    game.drawGrid
-
-    # win?
-    if game.win?(current_player == 1 ? game.p1 : game.p2)
-      puts "Result: Player #{current_player} win!"
-      break
-    # draw?
-    elsif game.no_more_moves?
-      puts 'Result: draw'
-      break
-    else
-      current_player == 1 ? current_player = 2 : current_player = 1
-    end
-
-  end
+  game.play
 
   puts 'Play again? [y/n]'
   input = gets.chomp
