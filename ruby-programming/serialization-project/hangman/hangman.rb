30f9cb762474a7a3fd72c83c9448b9f4bb771040
Lani Huang <laniywh@gmail.com>
2016-05-19 21:35:40 -0700

    add save/load feature

--- ruby-programming/serialization-project/hangman/hangman.rb
+++ ruby-programming/serialization-project/hangman/hangman.rb
@@ -1,16 +1,19 @@
+require 'date'
+require 'yaml'
+
 class Hangman
   TURNS = 5
 
   def initialize
-    @words = generate_words
+    words = generate_words
     @letter_bank = []
     @guesses_left = TURNS + 1
     @chosen_char = ""
+    @secret_word = words[rand words.length]
+    @show_char = Array.new(@secret_word.length) { |i| false }
   end
 
   def play
-    @secret_word = @words[rand @words.length]
-    @show_char = Array.new(@secret_word.length) { |i| false }
 
     loop do
 
@@ -26,9 +29,14 @@ class Hangman
         break
       end
 
-      # prompt for guess
-      print ": "
+
+      print "Guess: "
       @chosen_char = gets.chomp.downcase
+
+      if @chosen_char == 'save'
+        save
+        break
+      end
     end
   end
 
@@ -72,8 +80,44 @@ class Hangman
   def print_letter_bank
     @letter_bank.join(", ")
   end
+end
+
+
+
+
+def save
+  yaml = YAML::dump(self)
+  Dir.mkdir("save_games") unless Dir.exists? "save_games"
+  filename = "save_games/#{DateTime.now.to_s}.sav"
+  save_file = File.new(filename, 'w')
+  save_file.write(yaml)
+end
+
+def load
+
+  # get all game files
+  game_files = Dir.entries("save_games").select { |f| f.include?(".sav") }
+  game_files.each_with_index do |filename, index|
+    puts "[#{index}] #{filename}"
+  end
+
+  print ": "
+  index = gets.chomp
+  game_file = "save_games/#{game_files[index.to_i]}"
+  yaml = File.open(game_file).read
+  puts yaml
+  YAML::load(yaml)
 
 end
 
-game = Hangman.new
+puts "[N]ew Game   [L]oad Game"
+input = ""
+loop do
+  print ": "
+  input = gets.chomp.downcase
+  break if input == 'n' || input == 'l'
+  puts "Invalid input! Type 'n' or 'l'"
+end
+
+game = input == 'l' ? load : Hangman.new
 game.play
