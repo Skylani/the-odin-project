8e0dc320811d3799688b9bd222848ae67b3465ea
Lani Huang <laniywh@gmail.com>
2016-06-12 16:13:19 -0700

    initial commit

---
+++ html5-css3/grid-framework/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('autoprefixer'),
+    postcss = require('gulp-postcss'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
+    // livereload = require('gulp-livereload');
+
+function errorlog(error) {
+    console.error.bind(error);
+    this.emit('end');
+}
+
+gulp.task('styles', function() {
+    var processors = [
+      autoprefixer({browsers:['last 2 version']})
+    ];
+
+    gulp.src('*.scss')
+    .pipe(sass({
+        styles: 'expanded'
+    })).on('error', errorlog)
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dest'))
+    .pipe(browserSync.stream());
+
+});
+
+gulp.task('watch', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('*.scss', ['styles']);
+    gulp.watch('*.html').on("change", reload);
+});
+
+gulp.task('default', ['watch']);
